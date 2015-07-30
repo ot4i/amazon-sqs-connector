b8c17887c73b6c2fe9e980539a7923e793703ddb
Oliver Wynn <oli@uk.ibm.com>
2015-07-30 11:20:47 +0100

    Added copyright statements.  OW

--- AmazonSQS/src/com/ibm/connectors/AmazonSQS/AmazonSQSOutputConnector.java
+++ AmazonSQS/src/com/ibm/connectors/AmazonSQS/AmazonSQSOutputConnector.java
@@ -1,3 +1,15 @@
+/*
+Copyright 2015 IBM Corporation 
+Author Oliver Wynn
+ 
+  All rights reserved. This program and the accompanying materials
+  are made available under the terms of the MIT License
+  which accompanies this distribution, and is available at
+  http://opensource.org/licenses/MIT
+ 
+  Contributors:
+      Oliver Wynn - initial implementation v0.1
+*/
 package com.ibm.connectors.AmazonSQS;
 
 import com.ibm.connectors.AbstractOutputConnector;
